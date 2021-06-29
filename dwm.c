c3c0ec871791ff5b6a6db289fe18a35985e68705
Geet Sethi <geetsethi05@gmail.com>
2021-06-29 20:02:51 +0530

    🚧 Removed the changing of the `monocle` layout symbol
    The symbol for the monocle laout would change according to the number of
    windows open by putting a number between brackets(`[%d]`). Now the
    symbol remains the same one that is assigned to the layout in the
    layouts array.

--- dwm.c
+++ dwm.c
@@ -1408,14 +1408,8 @@ maprequest(XEvent *e)
 void
 monocle(Monitor *m)
 {
-	unsigned int n = 0;
 	Client *c;
 
-	for (c = m->clients; c; c = c->next)
-		if (ISVISIBLE(c))
-			n++;
-	if (n > 0) /* override layout symbol */
-		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
 	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
 		resize(c, m->wx, m->wy, m->ww, m->wh, 0, 0);
 }
